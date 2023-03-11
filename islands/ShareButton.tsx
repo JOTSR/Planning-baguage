d1e554b465abd165709bd0292f821d57d36532f7
Julien Oculi <xdrz.jot.sr@gmail.com>
2023-03-11 19:30:13 +0100

    feat: fallback for navigator.share

--- islands/ShareButton.tsx
+++ islands/ShareButton.tsx
@@ -1,17 +1,37 @@
 import { hydrate } from 'preact'
 import { Button } from '../components/Button.tsx'
+import { appendJsx } from '../utils.ts'
 import Toast from './Toast.tsx'
 
 async function share(shareData: ShareData) {
+	if ('share' in navigator) {
+		try {
+			await navigator.share(shareData)
+			appendJsx(<Toast type='success' message='Partagé' />, document.body)
+		} catch (error) {
+			appendJsx(
+				<Toast type='error' message={error.toString()} />,
+				document.body,
+			)
+		}
+		return
+	}
 	try {
-		await navigator.share(shareData)
-		hydrate(<Toast type='success' message='Partagé' />, document.body)
+		await (navigator as Navigator).clipboard.writeText(shareData.url!)
+		appendJsx(
+			<Toast type='success' message='Lien copié dans le presse papier' />,
+			document.body,
+		)
 	} catch (error) {
-		hydrate(
+		appendJsx(
 			<Toast type='error' message={error.toString()} />,
 			document.body,
 		)
 	}
+	appendJsx(
+		<Toast type='error' message='Impossible de partager le lien' />,
+		document.body,
+	)
 }
 
 type ShareButtonProps = {
