f6b92940dc3e1ffbb9d6d23e9ccdeb540c282281
Julien Oculi <xdrz.jot.sr@gmail.com>
2023-03-17 01:16:27 +0100

    refactor!: support more methods on DialogSubmitter

--- islands/DialogSubmitter.tsx
+++ islands/DialogSubmitter.tsx
@@ -1,20 +1,26 @@
 import { JSX } from 'preact/jsx-runtime'
 import { Button } from '../components/Button.tsx'
-import Submitter from './Submitter.tsx'
+import Submitter, { SubmitterOptions } from './Submitter.tsx'
 
 export default function DialogSubmitter(
-	{ title, action, children, onClose, ...props }:
+	{ title, action, children, method, onClose, ...props }:
 		& JSX.HTMLAttributes<HTMLDialogElement>
-		& { title: string; action: string; onClose?: (e: CloseEvent) => void },
+		& {
+			title: string
+			action: string
+			method: SubmitterOptions['method']
+			onClose?: (e: CloseEvent) => void
+		},
 ) {
 	return (
 		//@ts-ignore onClose on dialog
 		<dialog {...props} onClose={onClose} className='dialog_submitter'>
 			<span className='dialog_submitter-title'>{title}</span>
 			<Submitter
-				method='DIALOG'
+				method={method}
 				action={action}
-				className='form-panel dialog_submitter-form'
+				type='DIALOG'
+				reload={false}
 			>
 				{children}
 			</Submitter>
