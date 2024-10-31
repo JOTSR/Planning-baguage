ebfde84f93914d804f3a0679afbc2c27a7e5418e
Julien Oculi <xdrz.jot.sr@gmail.com>
2024-11-01 00:15:10 +0100

    hotfix: outing date can be undefined

--- islands/OutingsGallery.tsx
+++ islands/OutingsGallery.tsx
@@ -51,7 +51,7 @@ export default function OutingsGallery(
 				<div className='outings-gallery-session'>
 					<p className='outings-gallery-session-date'>
 						{dateFormat.format(
-							new Date(outings[displayed].startDate),
+							new Date(outings[displayed]?.startDate ?? 0),
 						)}
 					</p>
 					<p>
